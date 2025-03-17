542b49960b1947b88c6522e7d0cd332c2a5b369b
Joseph <joseph@good-idea.studio>
2025-03-17 16:11:31 -0600

    chore(api): add open_api_spex
    WIP

--- star_chart/mix.exs
+++ star_chart/mix.exs
@@ -32,6 +32,7 @@ defmodule StarChart.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:open_api_spex, "~> 3.21"},
       {:phoenix, "~> 1.7.14"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
